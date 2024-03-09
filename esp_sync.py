8301f8e6e529ed6fa163d97b7421d2c433a7c2a7
bkosciow <kosci1@gmail.com>
2024-03-09 20:36:42 +0100

    log level info

--- esp_sync.py
+++ esp_sync.py
@@ -34,7 +34,7 @@ import logging
 
 
 logging.basicConfig(
-    level=logging.DEBUG,
+    level=logging.INFO,
     format='%(asctime)s [%(levelname)s] %(message)s'
 )
 logger = logging.getLogger(__name__)
